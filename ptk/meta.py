dedc0a28f7ee86633010dc8413f913895ade8a3c
Jérôme Laheurte <271051+fraca7@users.noreply.github.com>
2015-12-25 16:58:44 +0100

    Fix URLs

--- ptk/meta.py
+++ ptk/meta.py
@@ -11,8 +11,8 @@ class PackageInfo(object):
     version_info = map(int, version.split(six.u('.')))
 
     project_name = six.u('ptk')
-    project_url = six.u('https://jeromelaheurte.net/ptk/')
-    download_url = six.u('https://jeromelaheurte.net/ptk/downloads/ptk-%s.tar.bz2') % version
+    project_url = six.u('https://bitbucket.org/fraca7/ptk')
+    download_url = six.u('https://pypi.python.org/packages/source/p/ptk/ptk-%s.tar.gz') % version
 
     author_name = six.u('J\u00E9r\u00F4me Laheurte')
     author_email = six.u('jerome@jeromelaheurte.net')
