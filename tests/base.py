64ecd6da5fac320144c237a0f9586f2ae79b45fe
Jérôme Laheurte <271051+fraca7@users.noreply.github.com>
2016-01-13 17:48:42 +0100

    Don't log warnings/info when unit testing

--- tests/base.py
+++ tests/base.py
@@ -2,3 +2,6 @@
 
 import sys, os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+
+import logging
+logging.basicConfig(level=logging.ERROR, format='%(asctime)-15s %(levelname)-8s %(name)-15s %(message)s')
