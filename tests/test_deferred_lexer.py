9c22a7c7b8f9b1dcb68d1fa4edfecf44d49ac144
Jérôme Laheurte <271051+fraca7@users.noreply.github.com>
2016-01-17 15:42:45 +0100

    Fix test

--- tests/test_deferred_lexer.py
+++ tests/test_deferred_lexer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import base, unittest
+import six
 
 from test_lexer import EOF, LexerBasicTestCaseMixin, PositionTestCaseMixin, TokenTypeTestCaseMixin, \
      LexerByteTestCaseMixin, LexerUnicodeTestCaseMixin, LexerConsumerTestCaseMixin, \
@@ -25,6 +26,8 @@ class DeferredLexerTestCase(unittest.TestCase):
         return succeed(None)
 
     def doLex(self, inputString):
+        if six.PY3 and isinstance(inputString, bytes):
+            inputString = [chr(c).encode('ascii') for c in inputString]
         for char in inputString:
             d = self.lexer.deferFeed(char)
             self.assertTrue(d.called)
